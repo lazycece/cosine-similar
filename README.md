a8a6e96a23b39353e802f31e482af3622223d427
lazycece <lazycece@gmail.com>
2019-07-26 11:13:22 +0800

    finish

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # cosine-similar
+
+## description
 使用词向量余弦算法计算文本相似度
+- 分词 [HanLP](https://github.com/hankcs/HanLP)
+- 词向量余弦计算公式
+  
+
+
+质能方程$$E = mc^2$$
+
+
+
+
+## apply
+```java
+public class Main {
+
+    public static void main(String[] args) {
+
+        // 高度相似文本
+        String text = "天气预报说，明天会下雨，你明天早上去上班的时候记得带上伞。";
+        String target = "你明天早上去上班的时候记得带上伞，天气预报说的可能会下雨。";
+
+        TextSimilarity textSimilarity = new TextSimilarity(text, target);
+        double similarity = textSimilarity.compute();
+
+        System.out.println(similarity);
+    }
+}
+```
--- src/main/java/com/lazycece/cosinesimilar/Main.java
+++ src/main/java/com/lazycece/cosinesimilar/Main.java
@@ -1,8 +1,34 @@
 package com.lazycece.cosinesimilar;
 
+import com.lazycece.cosinesimilar.apply.TextSimilarity;
+
 /**
  * @author lazycece
- * @date 2019/7/25
+ * @date 2019/7/26
  */
 public class Main {
+
+    public static void main(String[] args) {
+
+        // 高度相似文本
+        String text = "天气预报说，明天会下雨，你明天早上去上班的时候记得带上伞。";
+        String target = "你明天早上去上班的时候记得带上伞，天气预报说的可能会下雨。";
+
+        // 完全相似
+//        String text = "天气预报说，明天会下雨，你明天早上去上班的时候记得带上伞。";
+//        String target = "天气预报说，明天会下雨，你明天早上去上班的时候记得带上伞。";
+
+        //低相似
+//        String text = "天气预报说，明天会下雨，你明天早上去上班的时候记得带上伞。";
+//        String target = "夏天下雨很凉爽。";
+
+        //不相似
+//        String text = "冬天出太阳让人感觉暖洋洋";
+//        String target = "夏天下雨很凉爽。";
+
+        TextSimilarity textSimilarity = new TextSimilarity(text, target);
+        double similarity = textSimilarity.compute();
+
+        System.out.println(similarity);
+    }
 }
